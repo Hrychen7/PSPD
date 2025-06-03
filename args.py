fa0d5e9be453aa5b9b3473976a72ec71d58082af
Hrychen7 <127571868+Hrychen7@users.noreply.github.com>
2025-06-03 22:30:28 +0800

    Update args.py

--- args.py
+++ args.py
@@ -1,7 +1,7 @@
 import argparse
 
 def get_parser():
-    model_names = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152", "dense201", "m3t", "dbn", "vit", "bagnet"]
+    model_names = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152"]
 
     parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
     parser.add_argument('--data', metavar='DIR', default='/home/amax/data/yangyanwu/sub_mm', help='path to dataset')
@@ -11,7 +11,7 @@ def get_parser():
                         default='resnet18',
                         choices=model_names,
                         help='model architecture: ' + ' | '.join(model_names) + ' (default: resnet18)')
-    parser.add_argument('--epochs', default=160, type=int, metavar='N', help='number of total epochs to run')
+    parser.add_argument('--epochs', default=180, type=int, metavar='N', help='number of total epochs to run')
     parser.add_argument('--fold', default=0, type=int, help='fold number')
     parser.add_argument('--da',action='store_true', help='fold number')
     parser.add_argument('--pad',action='store_true', help='fold number')
@@ -25,12 +25,12 @@ def get_parser():
                         'using Data Parallel or Distributed Data Parallel')
     parser.add_argument('--lr',
                         '--learning-rate',
-                        default=3e-4,
+                        default=1e-6,
                         type=float,
                         metavar='LR',
                         help='initial learning rate',
                         dest='lr')
-    parser.add_argument('--local_rank', default=-1, type=int, help='node rank for distributed training')
+    parser.add_argument('--local-rank', default=-1, type=int, help='node rank for distributed training')
     parser.add_argument('--wd',
                         '--weight-decay',
                         default=0.00005,
@@ -52,11 +52,11 @@ def get_parser():
     parser.add_argument('--kd_weight', default=0.8, type=float, help='spl kd weight. ')
     parser.add_argument('--kd_mode', default=2, type=int, help='training mode. 0: KD, 1: pace_weigth, 2: ours ')
     parser.add_argument('--spl_type', default=1, type=int, help='training mode. 0: hard, 1: soft, 2: log ')
-    parser.add_argument('--ce_iter', default=0.003, type=int, help='ce iter weight. ')
+    parser.add_argument('--ce_iter', default=0.006, type=int, help='ce iter weight. ')
     parser.add_argument('--kd_iter', default=0.003, type=int, help='kd iter weight. ')
     
     
-    parser.add_argument('--mode', default=0, type=int, help='training mode. 0: mse, 1: dex, 2: dldl. 0/1/2. +3 == ours. ')
+    parser.add_argument('--mode', default=0, type=int, help='training mode.' )
     parser.add_argument('--env_name', default = "default", help='name for env')
     parser.add_argument('--opt_level', default = "O1", help='opt level, O1 default')
     parser.add_argument('--samples', '-s', default = 200, type=int, help='opt level, O1 default')
